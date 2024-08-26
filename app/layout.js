e986d654e09fc99430138a04063c2c29bc6e6a6e
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-26 15:39:49 +0300

    new

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
